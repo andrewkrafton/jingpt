bcea49be5c0bee44586056fe98140718e2ff2a26
andrewkrafton <andrew@krafton.com>
2026-01-08 17:57:30 +0900

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,28 +1,6 @@
 import NextAuth from "next-auth";
-import AzureADProvider from "next-auth/providers/azure-ad";
+import { authOptions } from "@/lib/auth";
 
-const handler = NextAuth({
-  providers: [
-    AzureADProvider({
-      clientId: process.env.AZURE_CLIENT_ID || "",
-      clientSecret: process.env.AZURE_CLIENT_SECRET || "",
-      tenantId: process.env.AZURE_TENANT_ID,
-      authorization: { params: { scope: "openid profile email Files.Read.All Sites.Read.All" } },
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, account }) {
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      return token;
-    },
-    async session({ session, token }: any) {
-      session.accessToken = token.accessToken;
-      return session;
-    },
-  },
-  secret: process.env.NEXTAUTH_SECRET,
-});
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
